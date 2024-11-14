b7fb8cc25ed6215059201bd7a83cfbcb7ba28dae
Shadyar Bakr <Shadyar.bakr@proton.me>
2024-11-14 13:16:58 +0300

    feat: add HTTP status code and header

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,6 +7,8 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+
 	w.Write([]byte("Hello from Go Snippet"))
 }
 
@@ -17,8 +19,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -26,5 +27,6 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new snippet..."))
 }

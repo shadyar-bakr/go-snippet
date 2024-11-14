762cb3c73e78421fceef39c42c4be7bb8b65eaf4
Shadyar Bakr <Shadyar.bakr@proton.me>
2024-11-14 13:46:57 +0300

    refactor: seperate routes to routes file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,24 +21,9 @@ func main() {
 		logger: logger,
 	}
 
-	// Server
-	mux := http.NewServeMux()
-
-	// Static files
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// GET
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// POST
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("Server Started", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }

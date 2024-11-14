4bea83bd8733544a9cc4f2f8273a360723b96f70
Shadyar Bakr <Shadyar.bakr@proton.me>
2024-11-14 13:07:41 +0300

    feat: add snippet view and create handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,5 +3,13 @@ package main
 import "net/http"
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Go Snippet"))
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,10 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on http://localhost:4000")
 

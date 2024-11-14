2df8fd6a7f6886eaac039b34dc70c4fecdc8824b
Shadyar Bakr <Shadyar.bakr@proton.me>
2024-11-14 13:33:56 +0300

    feat: add static file serving and static assets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,14 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	// Static files
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	// GET
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	// POST
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)

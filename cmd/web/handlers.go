be0e153644d274217724ceff77336dbd558159c9
Shadyar Bakr <Shadyar.bakr@proton.me>
2024-11-14 13:10:56 +0300

    feat: add id wildcard to snippet/view

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,13 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {

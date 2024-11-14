b0813a5fee0ccb1cdf12da59e935d8bd77a367fe
Shadyar Bakr <Shadyar.bakr@proton.me>
2024-11-14 18:54:36 +0300

    feat: use Alice middleware chain in routes
    - Replace individual middleware application with Alice middleware chain
    - Update `routes()` function to use the Alice middleware chain
    - Add `github.com/justinas/alice` dependency

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
@@ -24,5 +26,7 @@ func (app *application) routes() http.Handler {
 	// POST
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return standard.Then(mux)
 }
